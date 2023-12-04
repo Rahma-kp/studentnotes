a197aade081a9b7720e85f56469e88716f9bc577
Raham-kp <rahmakundupuzakkal7384@gmail.com>
2023-12-04 17:21:24 +0530

    changes

--- lib/screens/chart_screen.dart
+++ lib/screens/chart_screen.dart
@@ -1,4 +1,7 @@
+// pie_chart.dart
 import 'package:flutter/material.dart';
+import 'package:studentnot/db/db_functions/db_note_function.dart';
+import 'package:studentnot/db/db_model/note_db.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
 class PieChart extends StatelessWidget {
@@ -6,24 +9,43 @@ class PieChart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Map<String, int> calculateCategoryCounts(List<notesData> notes) {
+      Map<String, int> categoryCounts = {};
+      for (var note in notes) {
+        String category = note.category ?? "Other";
+        categoryCounts[category] = (categoryCounts[category] ?? 0) + 1;
+      }
+      return categoryCounts;
+    }
+
+    List<notesData> noteList = noteListNotifier.value;
+    Map<String, int> categoryCounts = calculateCategoryCounts(noteList);
+
+   List<_PieData> pieData = categoryCounts.entries
+    .map((MapEntry<String, int> entry) =>
+        _PieData(entry.key, entry.value.toDouble(), '${entry.value}%'))
+    .toList();
+
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: Text('chart',style: TextStyle(color: Colors.white),),
+          title: Text('𝐂𝐇𝐀𝐑𝐓', style: TextStyle(color: Colors.white)),
           backgroundColor: const Color.fromARGB(207, 13, 20, 78),
         ),
         body: SfCircularChart(
-            legend: const Legend(isVisible: true),
-            series: <PieSeries<_PieData, String>>[
-              PieSeries<_PieData, String>(
-                  explode: true,
-                  explodeIndex: 0,
-                  dataSource: pieData,
-                  xValueMapper: (_PieData data, _) => data.xData,
-                  yValueMapper: (_PieData data, _) => data.yData,
-                  dataLabelMapper: (_PieData data, _) => data.text,
-                  dataLabelSettings: const DataLabelSettings(isVisible: true)),
-            ]),
+          legend: const Legend(isVisible: true),
+          series: <PieSeries<_PieData, String>>[
+            PieSeries<_PieData, String>(
+              explode: true,
+              explodeIndex: 0,
+              dataSource: pieData,
+              xValueMapper: (_PieData data, _) => data.xData,
+              yValueMapper: (_PieData data, _) => data.yData,
+              dataLabelMapper: (_PieData data, _) => data.text,
+              dataLabelSettings: const DataLabelSettings(isVisible: true),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -35,15 +57,3 @@ class _PieData {
   final num yData;
   final String text;
 }
-
-final List<_PieData> pieData = [
-  _PieData('ENGLISH', 30, '30%'),
-  _PieData('LANGUAGE', 50, "50%"),
-  _PieData('PHYSICS', 30, '30%'),
-  _PieData('MATHEMATICS', 20, '20%'),
-  _PieData('CHEMISTRY', 15, '15%'),
-  _PieData('SOCIAL SCIENCE', 10, '10%'),
-  _PieData('BIOLOGY', 25, '25%'),
-  _PieData('ZOOLOGY', 45, '45%'),
-  _PieData('BOTANY', 20, '20%'),
-];
